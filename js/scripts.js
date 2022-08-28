ea0de0ef60f0fa9cf320d3c0797c00eda566b9ff
OLGON92 <ogon318@gmail.com>
2022-08-27 19:24:33 -0700

    add an if else statement.

--- js/scripts.js
+++ js/scripts.js
@@ -6,4 +6,22 @@ function langSuggester(event) {
   const userExerChoice = document.querySelector("input[name='phys-ed']:checked").value;
   const userStarWarsOrTrek = document.querySelector("input[name='wars-or-trek']:checked").value;
   const userFootyOrEggball = document.querySelector("input[name='footy-or-eggball']:checked").value;
+
+  let result;
+  if (userFaveDino === "t-rex" && userFalloutChoice === "fnv" && userExerChoice === "strength" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "footy") {
+    result ="Python";
+  } else if (userFaveDino === "raptor" && userFalloutChoice === "fallout-4" && userExerChoice === "cardio" && userStarWarsOrTrek === "star-trek" && userFootyOrEggball === "eggball") {
+    result ="C#";
+  } else if (userFaveDino === "no-dino-luv" && userFalloutChoice === "no-fallout-luv" && userExerChoice === "no-exercise" && userStarWarsOrTrek === "not-a-star-fan" && userFootyOrEggball === "no-sports") {
+    result ="Go";
+  } else if (userFaveDino === "t-rex" && userFalloutChoice === "fallout-4" && userExerChoice === "no-exercise" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "eggball") {
+    result ="JavaScript";
+  } else if (userFaveDino === "raptor" && userFalloutChoice === "fnv" && userExerChoice === "cardio" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "eggball") {
+    result ="Ruby";
+  } else if (userFaveDino === "no-dino-luv" && userFalloutChoice === "fallout-4" && userExerChoice === "strength" && userStarWarsOrTrek === "not-a-star-fan" && userFootyOrEggball === "eggball") {
+    result ="Rust";
+  } else {
+    result= "Swift";
+  }
+  
 }
