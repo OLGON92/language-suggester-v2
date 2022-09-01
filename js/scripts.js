0f58e15a50b52391dac23fd72d245fa0f3ef1482
OLGON92 <ogon318@gmail.com>
2022-08-31 18:32:20 -0700

    get rid of jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,49 @@
 //User Interface Logic
-$(document).ready(function() {
-  $("form#lang-suggester").submit(function(event) {
-    event.preventDefault();
-    const userFaveDino =$("input[name='fav-dino']:checked").val();
-    const userFalloutChoice =$("input[name='fallout-choice']:checked").val();
-    const userExerChoice =$("input[name='phys-ed']:checked").val();
-    const userStarWarsOrTrek =$("input[name='wars-or-trek']:checked").val();
-    const userFootyOrEggball =$("input[name='footy-or-eggball']:checked").val();
 
-  let result;
+  
+function quizQuestions(event) {
+  event.preventDefault();
+    const userFaveDino =document.querySelector("input[name='fav-dino']:checked").value;
+    const userFalloutChoice =document.querySelector("input[name='fallout-choice']:checked").value;
+    const userExerChoice =document.querySelector("input[name='phys-ed']:checked").value;
+    const userStarWarsOrTrek =document.querySelector("input[name='wars-or-trek']:checked").value;
+    const userFootyOrEggball =document.querySelector("input[name='footy-or-eggball']:checked").value;
+
   if (userFaveDino === "t-rex" && userFalloutChoice === "fnv" && userExerChoice === "strength" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "footy") {
-    result ="Python";
+    document.querySelector("span#myLang").innerText ="Python";
   } else if (userFaveDino === "raptor" && userFalloutChoice === "fallout-4" && userExerChoice === "cardio" && userStarWarsOrTrek === "star-trek" && userFootyOrEggball === "eggball") {
-    result ="C#";
+    document.querySelector("span#myLang").innerText ="C#";
   } else if (userFaveDino === "no-dino-luv" && userFalloutChoice === "no-fallout-luv" && userExerChoice === "no-exercise" && userStarWarsOrTrek === "not-a-star-fan" && userFootyOrEggball === "no-sports") {
-    result ="Go";
+    document.querySelector("span#myLang").innerText ="Go";
   } else if (userFaveDino === "t-rex" && userFalloutChoice === "fallout-4" && userExerChoice === "no-exercise" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "eggball") {
-    result ="JavaScript";
+    document.querySelector("span#myLang").innerText ="JavaScript";
   } else if (userFaveDino === "raptor" && userFalloutChoice === "fnv" && userExerChoice === "cardio" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "eggball") {
-    result ="Ruby";
+    document.querySelector("span#myLang").innerText ="Ruby";
   } else if (userFaveDino === "no-dino-luv" && userFalloutChoice === "fallout-4" && userExerChoice === "strength" && userStarWarsOrTrek === "not-a-star-fan" && userFootyOrEggball === "eggball") {
-    result ="Rust";
+    document.querySelector("span#myLang").innerText ="Rust";
   } else {
-    result= "Swift";
+    document.querySelector("span#myLang").innerText ="Swift";
   }
-
-  $("#result").text(result);
-    $("#results-here").show();
-
-  });
+}
+window.addEventListener("load", function()  { 
+  const form = document.getElementById("lang-suggester");
+  form.addEventListener("submit", quizQuestions)
 });
- 
+
+  
+
+
+/*window.addEventListener("load", function()  {
+  let form = document.querySelector("lang-suggester");
+  let resetBtn = document.querySelector("button#reset");
+  let resultsHere = document.querySelector("div#resultsHere");*/
+
+  /*resultsHere.removeAttribute("class");
+  event.preventDefault();
+
+  form.addEventListener("submit", function()  {
+    resetBtn.removeAttribute("class");
+
+    resetBtn.addEventListener("click", function() {
+      resultsHere.setAttribute("class", "hidden");
+      document.getElementById("span#myLang").value = null;*/
