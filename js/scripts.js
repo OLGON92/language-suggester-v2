38e02d23d89d10a9d87fb1920bde851c42e7f5bc
OLGON92 <ogon318@gmail.com>
2022-09-03 21:19:31 -0700

    uitilize event handlers as per the request to do so.

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,55 @@
 //User Interface Logic
 
   
-function quizQuestions(event) {
-  event.preventDefault();
-    const userFaveDino =document.querySelector("input[name='fav-dino']:checked").value;
-    const userFalloutChoice =document.querySelector("input[name='fallout-choice']:checked").value;
-    const userExerChoice =document.querySelector("input[name='phys-ed']:checked").value;
-    const userStarWarsOrTrek =document.querySelector("input[name='wars-or-trek']:checked").value;
-    const userFootyOrEggball =document.querySelector("input[name='footy-or-eggball']:checked").value;
 
+function quizAnswers()  {
+  const userFaveDino = document.querySelector("input[name='fav-dino']:checked").value;
+  const userFalloutChoice = document.querySelector("input[name='fallout-choice']:checked").value;
+  const userExerChoice = document.querySelector("input[name='phys-ed']:checked").value;
+  const userStarWarsOrTrek = document.querySelector("input[name='wars-or-trek']:checked").value;
+  const userFootyOrEggball = document.querySelector("input[name='footy-or-eggball']:checked").value;
+    let result = "span#myLang";
   if (userFaveDino === "t-rex" && userFalloutChoice === "fnv" && userExerChoice === "strength" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "footy") {
-    document.querySelector("span#myLang").innerText ="Python";
+    result = "Python";
   } else if (userFaveDino === "raptor" && userFalloutChoice === "fallout-4" && userExerChoice === "cardio" && userStarWarsOrTrek === "star-trek" && userFootyOrEggball === "eggball") {
-    document.querySelector("span#myLang").innerText ="C#";
+    result = "C#";
   } else if (userFaveDino === "no-dino-luv" && userFalloutChoice === "no-fallout-luv" && userExerChoice === "no-exercise" && userStarWarsOrTrek === "not-a-star-fan" && userFootyOrEggball === "no-sports") {
-    document.querySelector("span#myLang").innerText ="Go";
+    result = "Go";
   } else if (userFaveDino === "t-rex" && userFalloutChoice === "fallout-4" && userExerChoice === "no-exercise" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "eggball") {
-    document.querySelector("span#myLang").innerText ="JavaScript";
+    result = "JavaScript";
   } else if (userFaveDino === "raptor" && userFalloutChoice === "fnv" && userExerChoice === "cardio" && userStarWarsOrTrek === "star-wars" && userFootyOrEggball === "eggball") {
-    document.querySelector("span#myLang").innerText ="Ruby";
+    result = "Ruby";
   } else if (userFaveDino === "no-dino-luv" && userFalloutChoice === "fallout-4" && userExerChoice === "strength" && userStarWarsOrTrek === "not-a-star-fan" && userFootyOrEggball === "eggball") {
-    document.querySelector("span#myLang").innerText ="Rust";
+    result = "Rust";
   } else {
-    document.querySelector("span#myLang").innerText ="Swift";
+    result = "Swift";
   }
+  document.getElementById("suggestedLang").outerText = result;
+  event.preventDefault();
 }
-window.addEventListener("load", function()  { 
-  const form = document.getElementById("lang-suggester");
-  form.addEventListener("submit", quizQuestions)
-});
+function handleRadio(event) {
+  quizAnswers();
+
+}
+
+window.addEventListener("load", function() {
+  document.getElementById("lang-suggester").addEventListener("submit", handleRadio);
+  let form = document.getElementById("suggestedLang");
+  let submitBtn = document.getElementById("submitBtn");
+  let resetBtn = document.getElementById("resetBtn");
+  
+
+  submitBtn.removeAttribute("hidden");
+
+  form.addEventListener("submit", function() {
+    submitBtn.setAttribute("click","hidden");
+    resetBtn.removeAttribute("class");
+  });
 
   
 
 
-/*window.addEventListener("load", function()  {
-  let form = document.querySelector("lang-suggester");
-  let resetBtn = document.querySelector("button#reset");
-  let resultsHere = document.querySelector("div#resultsHere");*/
+});
 
-  /*resultsHere.removeAttribute("class");
-  event.preventDefault();
 
-  form.addEventListener("submit", function()  {
-    resetBtn.removeAttribute("class");
-
-    resetBtn.addEventListener("click", function() {
-      resultsHere.setAttribute("class", "hidden");
-      document.getElementById("span#myLang").value = null;*/
+  
